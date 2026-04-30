e9ec68b12d52526ce8117f95fa1e38153d3c4cd1
silkm <michael.silk@populationgenomics.org.au>
2026-04-30 17:32:22 +1000

    update sketchybar and aerospace

--- dot_config/sketchybar/plugins/executable_aerospace.sh
+++ dot_config/sketchybar/plugins/executable_aerospace.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 # Highlights the focused Aerospace workspace
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-  sketchybar --set "$NAME" background.drawing=on background.color=0x44ffffff background.corner_radius=4 background.height=22
+  sketchybar --set "$NAME" \
+    background.drawing=on \
+    background.color=0xffffffff \
+    background.height=2 \
+    background.corner_radius=0 \
+    background.y_offset=-10
 else
   sketchybar --set "$NAME" background.drawing=off
 fi
