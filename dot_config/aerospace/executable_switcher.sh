0b0325f5672325079a76407bfa7062029576f0fe
silkm <michael.silk@populationgenomics.org.au>
2026-05-05 20:21:42 +1000

    remove attempts at window management

--- dot_config/aerospace/executable_switcher.sh
+++ dot_config/aerospace/executable_switcher.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 export PATH="/opt/homebrew/bin:$PATH"
-sleep 0.2
-aerospace layout floating
-osascript <<'EOF'
-tell application "Finder"
-    set {sx, sy, sw, sh} to bounds of window of desktop
-end tell
-set ww to 900
-set wh to 500
-tell application "System Events" to tell process "Ghostty"
-    set size of window 1 to {ww, wh}
-    set position of window 1 to {(sw - ww) div 2, (sh - wh) div 2}
-end tell
-EOF
 
 selected=$(aerospace list-windows --all --format $'%{window-id}\t%{app-name}: %{window-title}' \
     | grep -v 'ae-switcher' \
