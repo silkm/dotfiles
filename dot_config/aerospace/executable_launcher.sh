c7ec3befe0a0c417ddc81dc0a97140f0047e5280
silkm <michael.silk@populationgenomics.org.au>
2026-05-05 20:41:51 +1000

    swap launcher order, rename meet to meethome

--- dot_config/aerospace/executable_launcher.sh
+++ dot_config/aerospace/executable_launcher.sh
@@ -2,11 +2,11 @@
 export PATH="/opt/homebrew/bin:$PATH"
 
 apps=$(find /Applications -maxdepth 1 -name "*.app" -exec basename {} .app \; | sort)
-selected=$(printf 'meet\nemacs\n%s' "$apps" | fzf --prompt='launch: ' --reverse)
+selected=$(printf 'emacs\nmeethome\n%s' "$apps" | fzf --prompt='launch: ' --reverse)
 
 case "$selected" in
-    meet)
-        open -na "Google Chrome" --args --profile-directory="Profile 3" "https://meet.google.com"
+    meethome)
+        open -na "Google Chrome" "https://meet.google.com"
         ;;
     emacs)
         open -n -a Emacs
